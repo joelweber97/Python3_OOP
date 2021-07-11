aae6d9c50e60cb95bae4288c5c22713f775f17cc
jweber <joel.weber97@gmail.com>
2021-07-10 22:00:45 -0400

    added compare and update method to the class

--- Telusko_OOP.py
+++ Telusko_OOP.py
@@ -32,8 +32,16 @@ class Computer:
         print('config is ', self.cpu, self.ram)
 
 
+    def update(self):
+        #the whole purpose of update method is to change the cpu of whatever it is called on to 'none'
+        self.cpu = 'none'
 
-
+    def compare(self, other):
+        if self.cpu == other.cpu:
+            return True
+        else:
+            return False
+        
 
 comp1 = Computer('i5', 16) #when you instantiate the object it will call init automatically.
 print(type(comp1))  #<class '__main__.Computer'>
@@ -41,6 +49,7 @@ print(comp1)
 print(comp1.cpu)
 print(comp1.ram)
 comp2 = Computer('Ryzen 3', 8)
+print(comp2)
 print(comp2.cpu)
 print(comp2.ram)
 
@@ -55,8 +64,20 @@ print(id(comp2))
 
 #in order to change the variables(attributes) of an object we simply do this
 comp1.cpu = 'Ryzen 9'
+print(comp1.cpu)
 
 comp1.config()
 comp2.config()
 
 
+#now we will use the update method to update the cpu for comp1
+comp1.update()
+print(comp1.cpu)
+print(comp2.cpu)
+
+
+#use the compare method to compare comp1 and comp2
+if comp1.compare(comp2):
+    print('they are the same')
+else:
+    print('they are different')
