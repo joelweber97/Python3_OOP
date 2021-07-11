cd9ed12a878da2519078a1468a33f514a2a76207
jweber <joel.weber97@gmail.com>
2021-07-10 21:43:18 -0400

    adding computer() related stuff

--- Telusko_OOP.py
+++ Telusko_OOP.py
@@ -38,11 +38,23 @@ class Computer:
 comp1 = Computer('i5', 16) #when you instantiate the object it will call init automatically.
 print(type(comp1))  #<class '__main__.Computer'>
 print(comp1)
+print(comp1.cpu)
+print(comp1.ram)
 comp2 = Computer('Ryzen 3', 8)
-
 print(comp2.cpu)
+print(comp2.ram)
+
+#each time you create an object it takes up different space in memory.
+#what is the size of an object? depends on the number of variables(attributes) of the object.
+#who allocates the size to an object? the constructor.
+#in this case the constructor is Computer() which calls __init__ automatically.
 print(id(comp1))
 print(id(comp2))
+
+
+
+#in order to change the variables(attributes) of an object we simply do this
+comp1.cpu = 'Ryzen 9'
 
 comp1.config()
 comp2.config()
