a6b450ee5869fde9517829cf976a22e2301693e1
Joel Weber <joel.weber97@gmail.com>
2022-11-05 05:45:07 +0100

    __init__ method

--- Telusko_OOP2.py
+++ Telusko_OOP2.py
@@ -18,7 +18,7 @@ attributes (variables) and behaviors (methods)
 ######
 # Class and Objects
 ######
-'''
+
 
 #the class is basically the blueprint of the objects an dthe objects are the instances
 class Computer:
@@ -40,13 +40,41 @@ comp1.config()
 comp2 = Computer()
 Computer.config(comp2)
 comp2.config()
+'''
+
 
 '''
 ######
 __init__ method in Python
 ######
+
+
+class Computer:
+
+    #we use __init__ to initialize the variables
+    #__init__ gets called automatically so we don't need to call the method
+    #like we would other methods
+    def __init__(self, cpu, ram): 
+        self.cpu = cpu
+        self.ram = ram
+
+    def config(self):
+        print(self.cpu, self.ram)
+
+#setting the cpu variable for comp1 to i5
+#and the ram variable for comp1 to 16
+comp1 = Computer('i5', 16) 
+#setting the cpu and ram variable for comp2 to r3 and 8
+comp2 = Computer("r3", 8)
+
+comp1.config() #returns i5, 16
+comp2.config() #returns r3, 8
+
 '''
 
+######
+#Constructor, self, and comparing objects in pythong
+######
 
 
 
